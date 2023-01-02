376d2c9105f998480b9ffec662fea893ebfa3190
hohfchns <hohfchns@gmail.com>
2023-01-02 11:27:45 +0200

    Removed commented code from fat bus

--- Core/Vendor/FatBus/fat_bus.cpp
+++ Core/Vendor/FatBus/fat_bus.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 
 namespace FatBus {
-static FATFS FatFs; 	//Fatfs handle
+static FATFS FatFs;
 
 FBus::FBus():
 _numHandles(0)
@@ -38,22 +38,6 @@ BusSuccess FBus::Init(ErrorCode& out_Error)
   if (out_Error != FR_OK) {
 	  return FAILURE;
   }
-
-  //Let's get some statistics from the SD card
-//  DWORD free_clusters, free_sectors, total_sectors;
-//
-//  FATFS* getFreeFs;
-//
-//  out_Error = f_getfree("", &free_clusters, &getFreeFs);
-//  if (out_Error != FR_OK) {
-//	  return FAILURE;
-//  }
-//
-//  //Formula comes from ChaN's documentation
-//  total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
-//  free_sectors = free_clusters * getFreeFs->csize;
-//
-////  myprintf("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
 
 	return SUCCESS;
  }
